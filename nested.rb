88fdd253d00a952a009855c2f37e53f1fd01ec7a
Natasha Chernis <natashachernis@gmail.com>
2019-02-27 02:54:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,8 +88,8 @@ def adding_matz
         }
      }
 
-	programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
-	binding.pry
+	programmer_hash[:yukihiro_matsumoto] = {}
+	
 	programmer_hash
 
 end
